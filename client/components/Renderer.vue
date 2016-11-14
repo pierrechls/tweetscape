6a7c0c3f6e256c50cc4cd3823503f0c82d45f1ff
Victor Viale <somebodywas@gmail.com>
2016-11-14 14:03:23 +0100

    Implement camera in scene

--- client/components/Renderer.vue
+++ client/components/Renderer.vue
@@ -3,19 +3,33 @@
     <a-scene gridhelper="size: 30;">
       <a-sphere :position="spherePositionAttr" :radius="sphere.radius" :color="sphere.color"></a-sphere>
       <a-plane :rotation="planeRotationAttr" :width="plane.dimensions.width" :height="plane.dimensions.height" :color="plane.color"></a-plane>
-      <a-entity :position="cameraPositionAttr">
-        <a-camera></a-camera>
-      </a-entity>
+      <camera :position="camera.position"></camera>
     </a-scene>
   </div>
 </template>
 
 <script>
+  import Camera from './Camera.vue'
+  import 'gsap/src/uncompressed/TweenMax'
+  import SimulationParams from '../params.js'
+  import Vector3D from 'utils/maths/vector3d.js'
 
   export default {
     name: 'Renderer',
+    components: {
+      'camera': Camera
+    },
     data: () => {
       return {
+        camera: {
+          position: {
+            x: 0,
+            y: 1.8,
+            z: 10
+          }
+        },
+        paths: [],
+        lastPath: new Vector3D(0,0,0),
         sphere: {
           position: {
             x: 1,
@@ -36,13 +50,6 @@
             z: 0
           },
           color: '#7BC8A4'
-        },
-        camera: {
-          position: {
-            x: 0,
-            y: 1.8,
-            z: 10
-          }
         }
       }
     },
@@ -58,6 +65,40 @@
       planeRotationAttr () {
         const rotation = this.plane.rotation;
         return `${rotation.x} ${rotation.y} ${rotation.z}`;
+      }
+    },
+    methods: {
+      drawPath: () => {
+        for(let i = 0; i < SimulationParams.path_amount_per_cycle; ++i) {
+          this.paths.push({x: i, y: 2*i, z: i+5})
+        } 
+
+        /*
+        For i in MAX_POINTS, draw points following math formulas
+         */
+      },
+      initLastPath: () => {
+        // TODO : get paths[0] and paths[1], get coeff_dir, set as base camera rot
+        this.lastPath = Vector3D.slope(this.paths[0], this.paths[1])
+      },
+      startSimulation: () => {
+        // TweenMax.to(this.camera.position, 5, {bezier:[{x:1, y:1, z: -25}, {x:0, y:2, z:-15}, {x:-1, y:1, z:-25}, {x:0, y:0, z:-15}, {x:1, y:1, z:-25}], ease: Linear.easeNone, repeat: -1});
+      }
+    },
+    mounted () {
+      setTimeout(function(){
+        this.drawPath()
+        this.initLastPath()
+        this.startSimulation()
+      }.bind(this), 
+      500)
+
+
+      TweenMax.to(this.camera.position, 5, {bezier:[{x:1, y:1, z: -25}, {x:0, y:2, z:-15}, {x:-1, y:1, z:-25}, {x:0, y:0, z:-15}, {x:1, y:1, z:-25}], ease: Linear.easeNone, repeat: -1});
+    },
+    watch: {
+      camera: function (camera) {
+        console.log(camera)
       }
     }
   }
