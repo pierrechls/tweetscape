014ba810afeefd96f80f163d438eae587c789f28
Victor Viale <somebodywas@gmail.com>
2016-11-14 14:03:12 +0100

    Add camera component

---
+++ client/components/Camera.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-entity :position="attributify(position)">
+    <a-camera></a-camera>
+  </a-entity>
+</template>
+
+<script>
+  import { attributify } from 'utils/aframe-utils.js'
+  
+  export default {
+    name: 'Camera',
+    props: {
+      position: Object
+    },
+    methods: {
+      attributify
+    }
+  }
+
+
+</script>
