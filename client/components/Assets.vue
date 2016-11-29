ffed0d5525b43a8a4f314f1cfd6ed438cf9d916f
Victor Viale <somebodywas@gmail.com>
2016-11-29 16:47:47 +0100

    Add assets components

---
+++ client/components/Assets.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-assets>
+    <canvas v-for="id in tweetTextureIds" :id="id" crossOrigin="anonymous"></canvas>
+  </a-assets>
+</template>
+
+<script>
+  export default {
+    name: 'Assets',
+    computed: {
+      tweets() {
+        return this.$store.state.tweets
+      },
+      tweetTextureIds() {
+        return this.tweets.map(t => t.id)
+      }
+    }
+  }
+</script>
