f96cb222b1e61937f3c09ed25b40239c4db26719
Victor Viale <somebodywas@gmail.com>
2016-11-29 16:46:57 +0100

    Add tweet component

---
+++ client/components/Tweet.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-plane :material="'shader: flat; src=#' + id"
+           :geometry="'width:' + tweetParams.width + ';height:' + tweetParams.height + ';'"
+           :position="attributify(position)"
+  >
+  </a-plane>
+</template>
+
+<script>
+  import { attributify } from 'utils/aframe-utils'
+  import SimulationParams from '../params.js'
+
+  export default {
+    name: 'Tweet',
+    props: {
+      position: Object,
+      id: String
+    },
+    computed: {
+      tweetParams () {
+        return SimulationParams.tweets
+      }
+    },
+    methods: {
+      attributify 
+    }
+  }
+
+</script>
