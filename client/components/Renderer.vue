c7833c4e6741b7c69ac1ea36f9a633d8228010b3
Victor Viale <somebodywas@gmail.com>
2016-11-29 16:48:56 +0100

    Fix assets rendering

--- client/components/Renderer.vue
+++ client/components/Renderer.vue
@@ -24,7 +24,8 @@
   export default {
     name: 'Renderer',
     components: {
-      'camera': Camera
+      'camera': Camera,
+      'assets': Assets,
       'tweet': Tweet
     },
     data: () => {
