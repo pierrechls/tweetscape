9124009d93a5896ec1f61ae1bad7d1e8e1c3bdfa
Victor Viale <somebodywas@gmail.com>
2016-11-29 16:45:36 +0100

    Add tweet size to params

--- client/params.js
+++ client/params.js
@@ -1,8 +1,16 @@
 const PATH_AMOUNT_PER_CYCLE = 150
+const TWEETS = {
+  width: 160,
+  height: 90
+}
 
 class SimulationParams {
   static get path_amount_per_cycle() {
     return PATH_AMOUNT_PER_CYCLE
+  }
+
+  static get tweets() {
+    return TWEETS
   }
 
 }
