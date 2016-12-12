d669de23406fb3b5f113e480ce94bef055f6794b
Victor Viale <somebodywas@gmail.com>
2016-12-12 18:36:21 +0100

    Componentify tweet canvas + draw

--- client/components/Assets.vue
+++ client/components/Assets.vue
@@ -1,18 +1,20 @@
 <template>
   <a-assets>
-    <canvas v-for="id in tweetTextureIds" :id="id" crossOrigin="anonymous"></canvas>
+    <tweet-canvas v-for="tweet in tweets" :tweet="tweet"></tweet-canvas>
   </a-assets>
 </template>
 
 <script>
+  import TweetCanvas from './TweetCanvas.vue'
+
   export default {
     name: 'Assets',
+    components: {
+      'tweet-canvas': TweetCanvas
+    },
     computed: {
       tweets() {
         return this.$store.state.tweets
-      },
-      tweetTextureIds() {
-        return this.tweets.map(t => t.id)
       }
     }
   }
