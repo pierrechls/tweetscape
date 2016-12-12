65f0434d3073473dd34cea097827f091719e90f5
Victor Viale <somebodywas@gmail.com>
2016-12-12 11:19:39 +0100

    Refactor flow of setHashtag

--- client/views/Hashtag.vue
+++ client/views/Hashtag.vue
@@ -24,14 +24,14 @@
     },
     methods: {
       start: function () {
-        this.$store.dispatch('setHashtag', this.userHashtag)
-
-        this.$http.get(`search/${this.userHashtag}`).then((response) => {
-          console.log('success: ', response.data)
-          this.$router.push({ path: '/' })
-        }, (response) => {
-          console.log('error: ', response)
-        })
+        this.$http.get(`search/${this.userHashtag}`)
+          .then((response) => {
+            console.log('success: ', response.data)
+            this.$store.dispatch('setHashtag', this.userHashtag)
+            this.$router.push({ path: '/' })
+          }, (response) => {
+            console.log('error: ', response)
+          })
       }
     }
   }
