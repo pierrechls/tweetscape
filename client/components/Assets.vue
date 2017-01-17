46107f5dd0bff7b4c38e33b5c68e9fd8024f057b
Pierre Charles <pierre.chls@gmail.com>
2017-01-17 15:17:45 +0100

    Remove canvas from assets

--- client/components/Assets.vue
+++ client/components/Assets.vue
@@ -1,21 +1,16 @@
 <template>
-  <a-assets>
-    <tweet-canvas v-for="tweet in tweets" :tweet="tweet"></tweet-canvas>
-  </a-assets>
+  <a-assets></a-assets>
 </template>
 
 <script>
-  import TweetCanvas from './TweetCanvas.vue'
 
   export default {
     name: 'Assets',
-    components: {
-      'tweet-canvas': TweetCanvas
-    },
     computed: {
       tweets() {
         return this.$store.state.tweets
       }
     }
   }
+
 </script>
