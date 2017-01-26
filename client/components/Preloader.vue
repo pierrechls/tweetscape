1d555df9911cce398e81b3313cd31220d57af3f6
Pierre Charles <pierre.chls@gmail.com>
2017-01-26 17:25:29 +0100

    Add props to Preloader component

--- client/components/Preloader.vue
+++ client/components/Preloader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="preloader">
+  <div class="preloader" :style="display ? 'display: table' : 'display: none'">
     <div class="preloader-center">
       <svg class="preloader-icon" width="34" height="38" viewBox="0 0 34 38">
         <path class="preloader-path" stroke-dashoffset="0" d="M29.437 8.114L19.35 2.132c-1.473-.86-3.207-.86-4.68 0L4.153 8.114C2.68 8.974 1.5 10.56 1.5 12.28v11.964c0 1.718 1.22 3.306 2.69 4.165l10.404 5.98c1.47.86 3.362.86 4.834 0l9.97-5.98c1.472-.86 2.102-2.45 2.102-4.168V12.28c0-1.72-.59-3.306-2.063-4.166z">
@@ -10,15 +10,34 @@
   </div>
 </template>
 
-<style>
+<script>
+
+  export default {
+    name: 'Preloader',
+    props: {
+      display: Boolean
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
 
   .preloader {
-    display : table;
+    /*display : table;*/
+    position: absolute;
+    z-index: 99;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     text-align : center;
     width: 100%;
     height: 100%;
     min-height: 230px;
     min-width: 230px;
+    opacity: 0;
+    transition: all 0.5s;
 
     &:before {
       display : inline-block;
