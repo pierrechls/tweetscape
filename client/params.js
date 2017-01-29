f4387b7507e64e9bad77a1ae84fe1d4dad6a60a0
Pierre Charles <pierre.chls@gmail.com>
2017-01-29 12:17:29 +0100

    Clean and add some params

--- client/params.js
+++ client/params.js
@@ -1,21 +1,48 @@
+// TWEET PARAMS
+const TWEET_SEPARATOR = 10
+const TWEET_SIZE = { width: 512, height: 512 }
+
+// PATH PARAMS
+const SPEED = 20
 const PATH_AMOUNT_PER_CYCLE = 100
-const SPEED = 1.0/10
-const TWEET_SIZE = {
-  width: 512,
-  height: 512
+const PATH_AMPLITUDE = {
+  x: { min: 1, max: 3 },
+  y: { min: 1, max: 2 }
+}
+const PATH_FREQUENCY = {
+  x: { min: 3, max: 5 },
+  y: { min: 3, max: 5 }
 }
 
+
 class SimulationParams {
-  static get path_amount_per_cycle() {
-    return PATH_AMOUNT_PER_CYCLE
-  }
+
+  // TWEET GETTERS
 
   static get tweetSize() {
     return TWEET_SIZE
   }
 
-  static get duration() {
-  	return 1.0/SPEED;
+  static get tweetSeparator() {
+  	return TWEET_SEPARATOR
+  }
+
+  // PATH GETTERS
+
+  static get speed() {
+  	return SPEED
+  }
+
+  static get pathAmountPerCycle() {
+    return PATH_AMOUNT_PER_CYCLE
+  }
+
+  static get pathAmplitude() {
+    return PATH_AMPLITUDE
+  }
+
+  static get pathFrequency() {
+    return PATH_FREQUENCY
   }
 
 }
