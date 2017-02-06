bef334744854bffc36201b4980ad841e74136a45
Pierre Charles <pierre.chls@gmail.com>
2017-02-06 14:33:18 +0100

    Add default settings (client/api config)

---
+++ settings/default.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "API": {
+    "url": "http://localhost",
+    "port": 3000
+  },
+  "client": {
+    "url": "http://localhost",
+    "port": 4000
+  },
+  "app": {
+    "tweet": {
+      "numberPerRequest": 100
+    }
+  }
+}
