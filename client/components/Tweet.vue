110bddb86250f2c6a6fb3ce7b8019eea3fb25467
Victor Viale <somebodywas@gmail.com>
2017-02-28 21:14:42 +0100

    remove test code

--- client/components/Tweet.vue
+++ client/components/Tweet.vue
@@ -31,48 +31,6 @@
     },
     mounted() {
 
-      // TODO if it's work without aframe-canvas :
-      //      - remove dependency into package.json
-      //      - remove import aframe-canvas into index.js file
-
-      /* *************************** */
-      /* TEST 1 : avec aframe-canvas */
-      /* *************************** */
-
-      // NOTE: to test it, please insert this prop into a-plane :
-      //       ` :canvas-material="'width:' + tweetParams.canvasWidth + ';height:' + tweetParams.canvasHeight + ';'" `
-
-      // window.setTimeout( () => {
-      //    let canvas = this.$el.components['canvas-material'].canvas
-      //    let tweetDrawer = new TweetDrawer(this.tweet, canvas)
-      //
-      //    tweetDrawer.draw().then( () => {
-      //      this.$el.components['canvas-material'].updateTexture()
-      //    })
-      // }, 100)
-
-      /* *************************** */
-      /* TEST 2 : sans aframe-canvas */
-      /* *************************** */
-
-      // window.setTimeout( () => {
-      //
-      //   let canvas = document.createElement('canvas')
-      //   canvas.width = 512
-      //   canvas.height = 512
-      //
-      //   let tweetDrawer = new TweetDrawer(this.tweet, canvas)
-      //
-      //   tweetDrawer.draw().then( () => {
-      //     let texture = new THREE.Texture(canvas)
-      //     let material = new THREE.MeshBasicMaterial({ map: texture, transparent: true, opacity: 0 })
-      //     this.$el.object3D.children[0].material = material
-      //     texture.needsUpdate = true
-      //
-      //   })
-      //
-      // }, 100)
-
       /* *********************************************** */
       /* TEST 3 : sans aframe-canvas + fade-in animation */
       /* *********************************************** */
