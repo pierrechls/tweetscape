86a480dff62d15c9a4f5a3d526a4a1f025fa8c71
Pierre Charles <pierre.chls@gmail.com>
2017-03-08 18:51:44 +0100

    :tada: Add EndMessage component

---
+++ client/components/EndMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-entity id="msg-end" :text="'opacity: 0; align: center; value:' + message + ';'" position="0 0 -1">
+    <a-animation attribute="text.opacity" begin="fade" to="1"></a-animation>
+  </a-entity>
+</template>
+
+<script>
+
+  export default {
+    name: 'EndMessage',
+    data () {
+      return {
+        message: 'EXPÉRIENCE FINIE'
+      }
+    }
+  }
+
+</script>
