cf0d57611b586d39e778e14bb214eeda90661c69
Pierre Charles <pierre.chls@gmail.com>
2017-03-10 19:06:18 +0100

    :sparkles: Load 3D models

--- client/components/Assets.vue
+++ client/components/Assets.vue
@@ -1,5 +1,9 @@
 <template>
-  <a-assets></a-assets>
+  <a-assets>
+    <a-asset-item id="logo-obj" src="~assets/obj/logo_quad.obj"></a-asset-item>
+    <a-asset-item id="logo-mtl" src="~assets/obj/logo_quad.mtl"></a-asset-item>
+    <a-asset-item id="logo-gltf" src="~assets/obj/logo-quad.gltf"></a-asset-item>
+  </a-assets>
 </template>
 
 <script>
