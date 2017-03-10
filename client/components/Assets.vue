426ae505e683c42579a51a2c3fc9a7e347a11bae
Pierre Charles <pierre.chls@gmail.com>
2017-03-10 22:59:32 +0100

    :sparkles: Load models assets

--- client/components/Assets.vue
+++ client/components/Assets.vue
@@ -1,8 +1,8 @@
 <template>
   <a-assets>
-    <a-asset-item id="logo-obj" src="~assets/obj/logo_quad.obj"></a-asset-item>
-    <a-asset-item id="logo-mtl" src="~assets/obj/logo_quad.mtl"></a-asset-item>
-    <a-asset-item id="logo-gltf" src="~assets/obj/logo-quad.gltf"></a-asset-item>
+    <a-asset-item id="logo-obj" src="/static/models/logo_quad.obj"></a-asset-item>
+    <a-asset-item id="logo-mtl" src="/static/models/logo_quad.mtl"></a-asset-item>
+    <a-asset-item id="logo-gltf" src="/static/models/logo-quad.gltf"></a-asset-item>
   </a-assets>
 </template>
 
