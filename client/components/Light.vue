89f88d3923eed87d42e55d50fa789cbc76cd7e41
Pierre Charles <pierre.chls@gmail.com>
2017-03-11 16:34:29 +0100

    :lipstick: Add light animation

--- client/components/Light.vue
+++ client/components/Light.vue
@@ -1,7 +1,9 @@
 <template>
   <a-entity>
     <a-entity light="type: ambient; color: #6B4BBA; intensity: 0.5;"></a-entity>
-    <a-entity light="type: spot; angle: 45; color: #21C2CC; penumbra: 1; distance: 17; intensity: 0.7; decay: -3;" :position="attributify(position)"></a-entity>
+    <a-entity id="spot-light" light="type: spot; angle: 45; color: #21C2CC; penumbra: 1; distance: 17; intensity: 0.7; decay: -3;" :position="attributify(position)">
+      <a-animation attribute="light.intensity" begin="turn-the-light-off" to="0"></a-animation>
+    </a-entity>
   </a-entity>
 </template>
 
