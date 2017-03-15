05f5a19f81c8ef61f8fe161946644a891d1a2253
Pierre Charles <pierre.chls@gmail.com>
2017-03-15 15:13:01 +0100

    :bug: Fix image shader

--- client/components/EndMessage.vue
+++ client/components/EndMessage.vue
@@ -1,6 +1,6 @@
 <template>
   <a-entity v-if="showEndMessage" position="0 0 -2">
-    <a-entity id="end-message-logo" geometry="primitive: plane; height: 1; width: 1;" material="opacity: 0; src: #logo-end-message; transparent: true;" position="0 0.5 0">
+    <a-entity id="end-message-logo" geometry="primitive: plane; height: 1; width: 1;" material="shader: flat; flatShading: true; opacity: 0; src: #logo-end-message; transparent: true;" position="0 0.5 0">
       <a-animation attribute="material.opacity" delay="1000" to="1"></a-animation>
     </a-entity>
     <a-entity id="end-message-text" scale="3 3 3" :text="'opacity: 0; align: center; value:' + message + ';'" position="0 -0.5 0">
