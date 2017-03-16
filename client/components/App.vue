d53dae8b21e6f0a358a2818fe59175614ff7d27c
Pierre Charles <pierre.chls@gmail.com>
2017-03-16 14:35:11 +0100

    :lipstick: Add font-size rem ratio

--- client/components/App.vue
+++ client/components/App.vue
@@ -34,6 +34,10 @@
 
   @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400i,600,600i,700,700i,900,900i');
 
+  html {
+    font-size: 100%;
+  }
+
   body {
     margin: 0;
     font-size: 2rem;
